2224dca1f3c42ee8d78dc7c834407ec79e0b87e8
Rubén Zúñiga <144453288+Ruben-Zuniga@users.noreply.github.com>
2024-06-23 17:59:40 -0300

    Update shiftreg.v

--- modulo_prueba/shiftreg.v
+++ modulo_prueba/shiftreg.v
@@ -1,27 +1,24 @@
-module shiftreg (
-
-    output      [3:0]   o_led,
+module shiftreg 
+(
+    output reg  [3:0]   o_led,
     input               i_valid,
     input               i_reset,
     input               clock
-
 );
 
 always @(posedge clock) begin
     
     if(!i_reset) begin
-        
-        if(i_valid) begin
-            
-            o_led << 1;
 
-        end
+        if(i_valid) 
+            o_led <= o_led << 1;
 
-    end else begin
-        
-        o_led << 4'b0;
+        else
+            o_led <= o_led;
 
-    end
+    end 
+    else 
+        o_led <= 4'b0;
 
 end
 
