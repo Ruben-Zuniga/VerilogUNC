a4c747804875a3021335420d5bb0d5fb5fdb5d34
Rubén Zúñiga <144453288+Ruben-Zuniga@users.noreply.github.com>
2024-06-23 18:00:51 -0300

    Update compare.v

--- modulo_prueba/compare.v
+++ modulo_prueba/compare.v
@@ -1,22 +1,25 @@
-module compare(
-
-    output              o_valid,
-    output              o_comp_reset,
-    input       [31:0]  i_count,
-    input               i_mux_data
-
+module compare
+#(
+    parameter   DATA_WIDTH = 32
+)
+(
+    output reg                    o_valid,
+    output reg                    o_comp_reset,
+    input       [DATA_WIDTH-1:0]  i_count_data,
+    input       [DATA_WIDTH-1:0]  i_mux_data
 );
 
-if() begin
+always@(*) begin
 
-    o_valid         = 1'b1;
-    o_comp_reset    = 1'b1;
+    if(i_count_data == i_mux_data) begin
+        o_valid         = 1'b1;
+        o_comp_reset    = 1'b1;
+        
+    end else begin
+        o_valid         = 1'b0;
+        o_comp_reset    = 1'b0;    
+    end
 
-end else begin
-
-    o_valid         = 1'b0;
-    o_comp_reset    = 1'b0;
-    
 end
 
 endmodule
