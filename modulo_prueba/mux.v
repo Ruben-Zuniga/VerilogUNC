16034ddef674f4aff3ccfe25cc2315fed88cb35e
Rubén Zúñiga <144453288+Ruben-Zuniga@users.noreply.github.com>
2024-06-23 17:59:57 -0300

    Update mux.v

--- modulo_prueba/mux.v
+++ modulo_prueba/mux.v
@@ -1,19 +1,26 @@
-module mux(
-
-    output              o_mux_data,
-    input       [1:0]   i_sw,
-    input       [3:0]   i_r
-
+module mux
+#(  
+    parameter   DATA_WIDTH = 32,
+    parameter   R0         = 3,
+    parameter   R1         = 10,
+    parameter   R2         = 100,
+    parameter   R3         = 5000
+ )
+(
+    output reg  [DATA_WIDTH-1:0] o_mux_data,
+    input       [1:0]            i_sw
 );
 
-case (i_sw)
+always@(*) begin
+    case (i_sw)
+    
+        2'b00:       o_mux_data = R0;
+        2'b01:       o_mux_data = R1;
+        2'b10:       o_mux_data = R2;
+        2'b11:       o_mux_data = R3;
+        default:     o_mux_data = {DATA_WIDTH{1'b0}};
 
-    2'b00:       o_mux_data = i_r[0];
-    2'b01:       o_mux_data = i_r[1];
-    2'b10:       o_mux_data = i_r[2];
-    2'b11:       o_mux_data = i_r[3];
-    default:     o_mux_data = 1'b1; 
-
-endcase
+    endcase
+end
 
 endmodule
