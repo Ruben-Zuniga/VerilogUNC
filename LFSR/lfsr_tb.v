aa72c5e6e1e3e0d8633d32ea09f4b47258f8ea7e
Ruben-Zuniga <rubenzu41@gmail.com>
2024-08-20 17:05:49 -0300

    Agregada la conexion de los dos modulos

--- LFSR/lfsr_tb.v
+++ LFSR/lfsr_tb.v
@@ -11,10 +11,12 @@ module LFSR16_1002D_tb;
     reg                             clk                                                         ;
 
     // Puertos
+    reg         [2:0]               n_test                                                      ;
     reg                             i_rst                                                       ;
     reg                             i_soft_rst                                                  ;
     reg                             i_valid                                                     ;
     reg         [LFSR_WIDTH-1:0]    i_seed                                                      ;
+    reg                             i_lfsr                                                      ;
     wire        [LFSR_WIDTH-1:0]    o_lfsr                                                      ;
     wire        [LFSR_WIDTH-1:0]    o_lfsr_checker                                              ;
     wire                            o_lock                                                      ;
@@ -22,14 +24,15 @@ module LFSR16_1002D_tb;
     wire        [1:0]               invalid_cnt                                                 ;
     wire        [2:0]               valid_cnt                                                   ;
 
-    // Seeds $random
-    integer                         s_soft_reset    = 100                                       ;
-    integer                         s_hard_reset    = 200                                       ;
-    integer                         s_valid         = 300                                       ;
-    integer                         delay                                                       ;
+    // Iterador
+    integer                         cnt                                                         ;
 
-    `define                         PERIODICITY                                                 ;
-    `define                         RANDOM_SEEDS                                                ;
+    // PERIODICITY o CHECKER
+    `define                         CHECKER;
+    // FIXED_SEEDS o RANDOM_SEEDS
+    `define                         RANDOM_SEEDS;
+    // TEST_1, TEST_2, TEST_3 o TEST_4
+    `define                         TEST_1;
 
     // Tarea que cambia el valor de i_seed
     task Change_seed
@@ -46,6 +49,7 @@ module LFSR16_1002D_tb;
         input   [LFSR_WIDTH-1:0]    new_seed 
     ); begin
 
+        $display("Soft reset at %0t ns", $time)                                             ;
         i_soft_rst                                  = 1'b1                                      ;
         Change_seed(new_seed)                                                                   ;
         #($urandom_range(200,1000))                                                             ;
@@ -76,10 +80,25 @@ module LFSR16_1002D_tb;
     // Asignacion de i_valid
     always@(posedge clk) begin
         if($urandom_range(0,1))
-            i_valid                                 <= 1'b1                                     ;
+            i_valid <= 1'b1                                                                     ;
         else
-            i_valid                                 <= 1'b0                                     ;
+            i_valid <= 1'b0                                                                     ;
     end
+
+    // Condiciones frontera del o_lock
+    `ifdef TEST_1
+        always@(*) i_lfsr = o_lfsr[LFSR_WIDTH-1];
+
+    `elsif TEST_2
+        always begin
+            if(!i_rst) begin
+                i_lfsr = o_lfsr[LFSR_WIDTH-1];
+                #
+                
+            end
+        end
+    `endif
+    
 
     initial begin
         $dumpfile("Modulos/LFSR/lfsr_tb.vcd");
@@ -90,16 +109,22 @@ module LFSR16_1002D_tb;
         i_seed                                      = LFSR_SEED                                 ;
         i_valid                                     = 1'b0                                      ;
         clk                                         = 1'b0                                      ;
+        i_lfsr                                      = 1'b0                                      ;
 
         #10000                                                                                  ;
         @(posedge clk)                                                                          ;
         i_rst                                       = 1'b0                                      ;
-
+        
         `ifdef PERIODICITY
             `include "tb_periodicity.v"
-        `else
-             //`include "tb_checker.v"
+        `elsif CHECKER
+            $display("\n-----o_lock test monitor-----")                                         ;
+            
+            $display("\nTEST 1: all valid")                                                     ;
+            $monitor("time: %0t\to_lock: %0b", $time, o_lock)                                   ;
 
+            `include "tb_periodicity.v"
+            
         `endif
                 
         #10000                                                                                  ;
@@ -132,7 +157,7 @@ module LFSR16_1002D_tb;
         .i_soft_rst     (i_soft_rst)                                                            ,
         .i_seed         (i_seed)                                                                ,
         .i_valid        (i_valid)                                                               ,
-        .i_lfsr         (o_lfsr[LFSR_WIDTH-1])
+        .i_lfsr         (i_lfsr)
     );
 
 endmodule
